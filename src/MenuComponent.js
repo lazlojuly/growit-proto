d05e44507c19de8803df417e948ad19dc23c9c4b
lazlojuly <lazlo.gy@gmail.com>
2016-11-17 13:29:03 +0000

    build for github pages

--- src/MenuComponent.js
+++ src/MenuComponent.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import Drawer from 'material-ui/Drawer'
+import Menu from 'material-ui/Menu'
 import MenuItem from 'material-ui/MenuItem'
 import AppBar from 'material-ui/AppBar'
 import Avatar from 'material-ui/Avatar'
+import Divider from 'material-ui/Divider'
 
 import profileRich from './assets/profile-rich.png'
+
+import ActionHome from 'material-ui/svg-icons/action/home'
+import ActionBuild from 'material-ui/svg-icons/action/build'
+import ActionDateRange from 'material-ui/svg-icons/action/date-range'
+import ActionTrendingUp from 'material-ui/svg-icons/action/trending-up'
+import ImageViewComfy from 'material-ui/svg-icons/image/view-comfy'
 
 export default class MenuComponent extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {open: false}
+
+    this.avatarStyle = {"marginBottom": 15}
+    this.menuStyle = {
+      paper: {
+        display: 'inline-block',
+        float: 'left',
+        margin: '16px 32px 16px 0',
+      },
+      rightIcon: {
+        textAlign: 'center',
+        lineHeight: '24px',
+      },
+    }
   }
 
   handleToggle = () => this.setState({open: !this.state.open})
@@ -27,12 +48,20 @@ export default class MenuComponent extends React.Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <Avatar src={profileRich} />
-          <MenuItem onTouchTap={this.handleClose}>Stock</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Jobs</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Plan</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Stats</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Products</MenuItem>
+
+          <Menu>
+            <MenuItem onTouchTap={this.handleClose}
+              leftAvatar={
+                <Avatar src={profileRich} />
+              }>
+            </MenuItem>
+            <Divider />
+            <MenuItem onTouchTap={this.handleClose} leftIcon={<ActionHome />}>Stock</MenuItem>
+            <MenuItem onTouchTap={this.handleClose} leftIcon={<ActionBuild />}>Jobs</MenuItem>
+            <MenuItem onTouchTap={this.handleClose} leftIcon={<ActionDateRange />}>Plan</MenuItem>
+            <MenuItem onTouchTap={this.handleClose} leftIcon={<ActionTrendingUp />}>Stats</MenuItem>
+            <MenuItem onTouchTap={this.handleClose} leftIcon={<ImageViewComfy />}>Products</MenuItem>
+          </Menu>
         </Drawer>
       </div>
     )
